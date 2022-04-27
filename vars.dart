e36fb75da857362fd22342d346ec835d7645a0c7
John Elder <johne4196@gmail.com>
2022-04-27 08:24:16 -0700

    variables

---
+++ vars.dart
@@ -0,0 +1,26 @@
+void main() {
+	// Variables
+	
+	// Strings
+	//var name = "John";
+	String name = "John";
+
+	// Integer
+	var x = 41;
+	//int x = 41;
+
+	// Dynamic
+	dynamic firstName = "Tim";
+
+	// Const and Final
+	const String fullName = "John Elder";
+	final String nickName = "J";
+
+	// declaring and not using
+	var myName;
+
+	print(myName);
+
+	myName = "John";
+	print(myName);
+}
