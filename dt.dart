651c31e31573109a0787b4bc0141c4832c4eaeba
John Elder <johne4196@gmail.com>
2022-05-02 08:47:23 -0700

    Data Types

---
+++ dt.dart
@@ -0,0 +1,27 @@
+void main() {
+	// Data Types
+	// Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+	// Strings
+	String firstName = "John";
+	print("String: $firstName");
+
+	// Integers
+	int myNum = 41;
+	print("Integer: $myNum");	
+
+	// Doubles
+	double otherNum = 19.95;
+	print("Double: $otherNum");
+
+	// Booleans
+	bool myBool = false;
+	print("Boolean: $myBool");
+
+	// Dynamic
+	dynamic fullName = "John Elder";
+	print("Dynamic: $fullName");
+
+
+
+}
---
+++ dt2.dart
@@ -0,0 +1,26 @@
+void main() {
+	// Data Types
+	// Numbers, Strings, Booleans, Lists, Maps
+	// Integers (int) Double (double) 
+	// Strings
+	// Booleans = bool
+	// dynamic, if the type is not specified, dart implies dynamic
+
+
+
+	String name = "John";
+	print("String: $name");
+
+	int myNum = 41;
+	print("Int: $myNum");
+
+	double otherNum = 41.95;
+	print("Double: $otherNum");
+
+	bool myBool = false;
+	print("Boolean: $myBool");
+
+	dynamic fullName = "John Elder";
+	print("Dynamic: $fullName");
+
+}
