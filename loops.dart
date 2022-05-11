ca878c7496a4c7efc21125688711710cd756fa86
John Elder <johne4196@gmail.com>
2022-05-11 08:54:34 -0700

    Loops

---
+++ loops.dart
@@ -0,0 +1,22 @@
+void main() {
+	// For Loop
+	var num = 5;
+	for (var i = num; i >= 1; i--) {
+		print(i);
+	}
+
+	// For In Loop
+	var names = ["John", "Tina", "Steve"];
+	for (var name in names){
+		print(name);
+	}
+
+	// While Loop
+	while(num >=1) {
+		print(num);
+		num--;
+	}
+
+
+
+}
