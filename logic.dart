370ac9004e1e82448b1e2b47f7ef65e35f343013
John Elder <johne4196@gmail.com>
2022-05-12 08:48:57 -0700

    Logic

---
+++ logic.dart
@@ -0,0 +1,16 @@
+void main() {
+	// Logic - If, If/Else, If Else If
+	var num = 51;
+	if (num == 5){
+		print("The Number Is 5!");
+	} 
+	else if (num == 3){
+		print("The Number is 3!");
+
+	} else {
+		print("The Number is NOT 3 or 5, it is $num");
+
+	}
+
+
+}
---
+++ logic2.dart
@@ -0,0 +1,15 @@
+void main() {
+	// If
+	var num = 31;
+	if (num == 5) {
+		print("The Number is 5!");
+	}
+	else if (num == 3){
+		print("The number is 3!");
+	}
+
+	else {
+		print("The Number is not 3 or 5!");
+	}
+
+}
