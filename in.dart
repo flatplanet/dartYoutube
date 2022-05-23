ecafe8d7bcb483642dfe182d9019b304432f6bc8
John Elder <johne4196@gmail.com>
2022-05-23 09:16:21 -0700

    User Input

---
+++ in.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+	// User Input
+	print("Enter Your Name:");
+
+	// allow the user to enter their Name
+	//var name = stdin.readLineSync();
+
+	String? name = stdin.readLineSync();
+	print("Hello $name");
+
+
+}
